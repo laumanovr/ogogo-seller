9ed4b423319e08d78bfe67b3ddb202c8dc810b00
Emir Azimov <emir.azimov@gmail.com>
2024-03-11 11:16:00 +0600

    maska installed

--- src/main.ts
+++ src/main.ts
@@ -12,67 +12,29 @@ import { RouteLocationNormalized } from "vue-router";
 
 interceptors();
 
-// router.beforeEach(
-//   (
-//     to: RouteLocationNormalized,
-//     from: RouteLocationNormalized,
-//     next: Function
-//   ) => {
-//     const isAuthenticated = Boolean(getItem("sessionId")),
-//       requiresAuth = to.matched.some((record) => record.meta.requiresAuth),
-//       roleScreensObj = getItem("auth")?.roleScreensObj ?? null,
-//       currentMenu = getItem("auth")?.uiElements ?? [];
+router.beforeEach(
+  (
+    to: RouteLocationNormalized,
+    from: RouteLocationNormalized,
+    next: Function
+  ) => {
+    const isAuthenticated = Boolean(getItem("sessionId")),
+      requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+    // roleScreensObj = getItem("auth")?.roleScreensObj ?? null,
+    // currentMenu = getItem("auth")?.uiElements ?? [];
 
-//     if (!isAuthenticated) {
-//       if (requiresAuth) {
-//         return next("/");
-//       }
-//       next();
-//     } else {
-//       // authenticated -> not authenticated
-//       if (to.path === "/" || to.path === "/forgot-password") {
-//         // redirect to first current menu or login with error
-//         if (currentMenu.length === 0) {
-//           next();
-//         } else {
-//           if (!roleScreensObj[to.meta.id]) {
-//             next(currentMenu[0].link);
-//           } else {
-//             next();
-//           }
-//         }
-//       } else {
-//         // redirect to first current menu or login with error
-//         if (currentMenu.length === 0) {
-//           next("/");
-//           return store.dispatch(
-//             notificationActionTypes.addErrorNotification,
-//             i18n.t("label-b7162593-1b0a-4c35-b4e9-845fb90a47ad")
-//           );
-//         } else {
-//           if (to.meta?.default) {
-//             next();
-//           } else if (!roleScreensObj[to.meta.id]) {
-//             if (to.fullPath.startsWith("/dashboard/params")) {
-//               const otherParamsPages = MENU_LIST.filter(
-//                 (e) => roleScreensObj[e.id]
-//               );
-//               if (otherParamsPages.length) {
-//                 next({ path: otherParamsPages[0].link });
-//               } else {
-//                 next({ name: currentMenu[0].link });
-//               }
-//             } else {
-//               next({ name: currentMenu[0].link });
-//             }
-//           } else {
-//             next();
-//           }
-//         }
-//       }
-//     }
-//   }
-// );
+    if (!isAuthenticated) {
+      if (requiresAuth) {
+        return next("/");
+      }
+      next();
+    } else if (to.path === "/" && isAuthenticated) {
+      next("/property");
+    } else {
+      next();
+    }
+  }
+);
 
 const app = createApp(App);
 
