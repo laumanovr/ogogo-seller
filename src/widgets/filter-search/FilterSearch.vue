cd1435393f55c0c99da0cb7664c357ffc3325b18
Ramazan <laumanov.r@gmail.com>
2024-03-11 16:54:02 +0600

    add filter-search component

---
+++ src/widgets/filter-search/FilterSearch.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="filter-search">
+    <SInput isSearchable width="80%" />
+    <SButton color="white" @click="onClick">
+      <SIconRender name="SettingsIcon" />
+      {{ $t("lang-7de4a879-828e-48b2-997c-310f0d6e0d75") }}
+    </SButton>
+    <SButton color="white">Скачать Excel</SButton>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { SInput, SButton, SIconRender } from "@tumarsoft/ogogo-ui";
+
+const emit = defineEmits(["onClick"]);
+
+const onClick = () => {
+  emit("onClick");
+};
+</script>
+
+<style lang="scss">
+.filter-search {
+  display: flex;
+  margin-bottom: 25px;
+
+  .button {
+    margin-left: 12px;
+    white-space: nowrap;
+  }
+}
+</style>
