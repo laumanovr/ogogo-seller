049af99699297151dfdedd1e25bd841484e47076
Ramazan <laumanov.r@gmail.com>
2024-03-11 11:05:02 +0600

    create components for orders

---
+++ src/features/order/ui/Order.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="order-content">Orders 7</div>
+</template>
+
+<script lang="ts" setup>
+import {
+  STable,
+  SButton,
+  SInput,
+  STabs,
+  STabItem,
+  STabWindow,
+} from "@tumarsoft/ogogo-ui";
+import { ref } from "vue";
+</script>
+
+<style lang="scss">
+.order-content {
+}
+</style>
---
+++ src/pages/order/ui/Order.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="order-container">
+    <Order />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Order } from "@/features/order";
+</script>
+
+<style lang="scss"></style>
