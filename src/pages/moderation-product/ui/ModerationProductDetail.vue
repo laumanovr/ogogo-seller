28ddf13ebb8a2366afe188ebf6254d33616bb55f
Ramazan <laumanov.r@gmail.com>
2024-03-12 12:47:03 +0600

    create moderation product detail

---
+++ src/pages/moderation-product/ui/ModerationProductDetail.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="moderation-product-detail">
+    <ModerationProductDetail />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ModerationProductDetail } from "@/features/moderation-product";
+</script>
