f0ce37c27b646c7147a1aa33246fdf36d4475693
Ramazan <laumanov.r@gmail.com>
2024-03-12 11:08:22 +0600

    create product pages

---
+++ src/features/product/ui/Product.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="product-content">
+    <div class="head-title">Каталог товаров</div>
+    <FilterSearch />
+    <EmptyData
+      text="Пока нет товаров"
+      button-title="Добавить товар"
+      @click="toggleModal"
+    />
+    <SModal :isModalOpen="isShowModal" @onClose="toggleModal">
+      Добавить товар
+    </SModal>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import EmptyData from "@/widgets/empty-data/EmptyData.vue";
+import FilterSearch from "@/widgets/filter-search/FilterSearch.vue";
+import { ref } from "vue";
+import { SModal } from "@tumarsoft/ogogo-ui";
+
+const isShowModal = ref(false);
+
+const toggleModal = () => {
+  isShowModal.value = !isShowModal.value;
+};
+</script>
+
+<style lang="scss"></style>
---
+++ src/pages/product/ui/Product.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="product-container">
+    <Product />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { Product } from "../../../features/product";
+</script>
