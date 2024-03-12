cc5229e527f9cf8e9e232eab72acbca839380336
Ramazan <laumanov.r@gmail.com>
2024-03-12 12:46:01 +0600

    add breadcrumbs

---
+++ src/widgets/Breadcrumbs/Breadcrumbs.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="breadcrumbs-container">
+    <div class="breadcrumb-item" v-for="(item, i) in items" :key="i">
+      <router-link :to="item.link">{{ item.name }}</router-link>
+      <span v-show="i !== items.length - 1">/</span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { reactive, PropType } from "vue";
+
+const props = defineProps({
+  items: {
+    type: Array as PropType<any[]>,
+    default: () => [],
+  },
+});
+
+let items = reactive(props.items);
+
+//TODO: Delete later
+items = [
+  { name: "Электроника", link: "#" },
+  { name: "Смартфоны и гаджеты", link: "#" },
+  { name: "Apple", link: "#" },
+  { name: "Iphone 15 pro", link: "#" },
+];
+</script>
+
+<style lang="scss">
+.breadcrumbs-container {
+  display: flex;
+  .breadcrumb-item {
+    display: flex;
+    font-weight: 600;
+    font-size: 16px;
+    a {
+      text-decoration: none;
+      color: #000;
+    }
+    span {
+      margin: 0 6px;
+    }
+  }
+}
+</style>
