0243bb4bd0f81b0edf67d92cf21920440f9539cc
Ramazan <laumanov.r@gmail.com>
2024-03-14 10:59:35 +0600

    add localization to product

--- src/widgets/products/ui/ProductWidget.vue
+++ src/widgets/products/ui/ProductWidget.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="product-content">
-    <div class="head-title">Каталог товаров</div>
+    <div class="head-title">
+      {{ $t("lang-6a3f0b82-3b93-4348-8788-a2ea2dcb2c88") }}
+    </div>
     <FilterSearch />
     <EmptyData
       text="Пока нет товаров"
-      button-title="Добавить товар"
+      :button-title="$t('lang-bb00cbbb-a6f7-4c77-8bf7-558b18e8d505')"
       @click="toggleModal"
     />
     <SModal :isModalOpen="isShowModal" @onClose="toggleModal" height="auto">
-      <p class="font-bold">Добавить товар</p>
+      <p class="font-bold">
+        {{ $t("lang-bb00cbbb-a6f7-4c77-8bf7-558b18e8d505") }}
+      </p>
       <SSelect
         :label="$t('lang-75805fdb-eac2-4c87-b481-563e318789b5')"
         class="w-p-100"
