db3a6a0b850cb1f7ea948969e4dcab9389cae60d
Ramazan <laumanov.r@gmail.com>
2024-03-27 17:38:12 +0600

    create product store

---
+++ src/widgets/products/store/product.store.ts
@@ -0,0 +1,34 @@
+import { defineStore } from "pinia";
+import { useAlertStore } from "@/shared/store/alert";
+import { useLoaderStore } from "@/shared/store/loader";
+import { IProduct, IProductResponse } from "./product-store.types";
+import { ProductApi } from "../api/product.api";
+const productApi = new ProductApi();
+const loaderStore = useLoaderStore();
+const alertStore = useAlertStore();
+
+export const useProductStore = defineStore("productStore", {
+  state: (): Partial<IProduct> => ({
+    products: [],
+  }),
+  getters: {},
+  actions: {
+    getAllProducts(payload: any): Promise<IProductResponse> {
+      return new Promise((resolve, reject) => {
+        loaderStore.setLoaderState(true);
+        productApi
+          .getProducts(payload)
+          .then((response) => {
+            this.products = response.items;
+            loaderStore.setLoaderState(false);
+            resolve(response);
+          })
+          .catch((err) => {
+            alertStore.showError(err.message);
+            loaderStore.setLoaderState(false);
+            reject(err);
+          });
+      });
+    },
+  },
+});
