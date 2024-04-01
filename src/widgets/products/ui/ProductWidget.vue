e10223605f4d7282e7fb7c1da0b1fa799705cb4e
Ramazan <laumanov.r@gmail.com>
2024-04-01 15:15:14 +0600

    add filter-modal to products

--- src/widgets/products/ui/ProductWidget.vue
+++ src/widgets/products/ui/ProductWidget.vue
@@ -6,7 +6,11 @@
     <template v-if="hasProducts">
       <div class="flex justify-between">
         <div class="flex w-p-65">
-          <FilterSearch class="w-p-80" @input="onSearch" />
+          <FilterSearch
+            class="w-p-80"
+            @input="onSearch"
+            @click="toggleFilterModal"
+          />
           <SButton color="white" class="ml-12">Настроить цены</SButton>
         </div>
         <SButton color="violet" @click="toggleModal">
@@ -75,11 +79,54 @@
       >
       </SSelect>
       <div class="flex flex-row w-p-100 gap-2 mt-36 justify-between">
-        <SButton color="gray" class="button w-p-49" @click="onClose">
+        <SButton color="gray" class="button w-p-49" @click="toggleModal">
           {{ $t("lang-c66fcd83-27ea-4c39-a1e9-be9c01dfdb36") }}
         </SButton>
         <SButton color="violet" class="button w-p-49">
           {{ $t("lang-d0108c3e-9945-4245-a730-b011e5a47c35") }}
+        </SButton>
+      </div>
+    </SModal>
+    <SModal
+      :isModalOpen="isOpenFilterModal"
+      class="filter-modal"
+      width="420px"
+      height="100%"
+      position="right"
+      @onClose="toggleFilterModal"
+    >
+      <div class="modal-content" ref="modalContent">
+        <div class="filter-title">
+          {{ $t("lang-7de4a879-828e-48b2-997c-310f0d6e0d75") }}
+        </div>
+
+        <div class="section">
+          <div class="section-title">Цена</div>
+          <div class="flex">
+            <SInput placeHolder="От" />
+            <SInput placeHolder="До" class="ml-8" />
+          </div>
+        </div>
+
+        <div class="section">
+          <div class="section-title">Категории</div>
+          <SInput isSearchable width="100%" />
+          <div
+            class="property-items"
+            :style="{ 'max-height': maxHeight + 'px' }"
+          >
+            <div v-for="item in 30" :key="item">
+              <SCheckbox>Смартфоны и телефоны</SCheckbox>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="filter-actions">
+        <SButton size="large" color="gray">
+          {{ $t("lang-7967cf86-49d6-41c2-bdd7-23c6f8e5e8ea") }}
+        </SButton>
+        <SButton size="large" color="violet">
+          {{ $t("lang-bdc9ab1e-91af-43ea-8bb9-e23c8ea98755") }}
         </SButton>
       </div>
     </SModal>
@@ -89,7 +136,7 @@
 <script lang="ts" setup>
 import { EmptyData } from "@/shared/ui/components/empty-data";
 import { FilterSearch } from "@/shared/ui/components/filter-search";
-import { ref, reactive, onMounted, computed } from "vue";
+import { ref, reactive, onMounted, computed, nextTick } from "vue";
 import i18n from "@/shared/lib/plugins/i18n";
 import {
   SModal,
@@ -99,6 +146,8 @@ import {
   STabItem,
   STable,
   SBadge,
+  SInput,
+  SCheckbox,
 } from "@tumarsoft/ogogo-ui";
 import { useProductStore } from "../store/product.store";
 
@@ -154,6 +203,9 @@ const hasStatusProducts = ref(false);
 const tab = ref("one");
 const totalItems = ref(0);
 const searchTimer = ref(null);
+const isOpenFilterModal = ref(false);
+const modalContent = ref(null);
+const maxHeight = ref(0);
 
 const currentStatus = computed(() =>
   Number(tab.value) ? { statuses: [Number(tab.value)] } : {}
@@ -201,8 +253,12 @@ const toggleModal = () => {
   isShowModal.value = !isShowModal.value;
 };
 
-const onClose = () => {
-  isShowModal.value = !isShowModal.value;
+const toggleFilterModal = () => {
+  isOpenFilterModal.value = !isOpenFilterModal.value;
+  nextTick(() => {
+    const rect = modalContent?.value?.getBoundingClientRect();
+    maxHeight.value = rect?.height / 2;
+  });
 };
 </script>
 
@@ -227,6 +283,79 @@ const onClose = () => {
       width: 44px;
       height: 44px;
       object-fit: scale-down;
+    }
+  }
+  .filter-modal {
+    .modal-content {
+      height: calc(100% - 75px);
+      overflow-y: auto;
+
+      &::-webkit-scrollbar {
+        width: 8px;
+      }
+
+      &::-webkit-scrollbar-thumb {
+        background-color: $gray-300;
+        border-radius: 4px;
+      }
+    }
+
+    .filter-title {
+      font-size: 24px;
+      font-weight: 700;
+      margin-bottom: 35px;
+    }
+
+    .section {
+      &:not(:last-child) {
+        margin-bottom: 30px;
+      }
+
+      &-title {
+        font-size: 16px;
+        font-weight: 700;
+        margin-bottom: 20px;
+      }
+    }
+
+    .checkbox-title {
+      font-size: 14px;
+      font-weight: 500;
+    }
+
+    .checkbox-container {
+      margin-bottom: 8px;
+    }
+
+    .property-items {
+      overflow-y: auto;
+      margin-top: 15px;
+
+      &::-webkit-scrollbar {
+        width: 8px;
+      }
+
+      &::-webkit-scrollbar-thumb {
+        background-color: $gray-300;
+        border-radius: 4px;
+      }
+    }
+
+    .input-container {
+      width: calc(100% - 10px);
+    }
+
+    .filter-actions {
+      display: flex;
+      justify-content: space-between;
+      padding: 24px 0 24px;
+      position: absolute;
+      bottom: 0;
+      width: calc(100% - 56px);
+
+      .button {
+        width: calc(50% - 4px);
+      }
     }
   }
 }
