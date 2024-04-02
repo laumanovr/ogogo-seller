9261b5cd505ad91fe4511e72fcc7c2973e3b8cb6
Ramazan <laumanov.r@gmail.com>
2024-04-02 16:47:59 +0600

    prepare show orders in orders

--- src/widgets/order/ui/Order.vue
+++ src/widgets/order/ui/Order.vue
@@ -6,31 +6,31 @@
     <FilterSearch />
 
     <STabs :tab-mode="'filter-tabs'" class="mb-20">
-      <STabItem value="one" :active-tab="tab" @changeTab="selectTab">
+      <STabItem value="14400" :active-tab="tab" @changeTab="selectTab">
         {{ $t("lang-dd1a9bc2-31f2-4197-9a7d-01a23229ea82") }}
       </STabItem>
-      <STabItem value="two" :active-tab="tab" @changeTab="selectTab">
+      <STabItem value="0" :active-tab="tab" @changeTab="selectTab">
         {{ $t("lang-7db32df9-54d2-4561-ba8b-c43073ee42e9") }}
       </STabItem>
-      <STabItem value="three" :active-tab="tab" @changeTab="selectTab">
+      <STabItem value="14401" :active-tab="tab" @changeTab="selectTab">
         {{ $t("lang-b5ea90d6-b263-4058-b295-b41b2d72c542") }}
       </STabItem>
-      <STabItem value="four" :active-tab="tab" @changeTab="selectTab">
+      <STabItem value="14402" :active-tab="tab" @changeTab="selectTab">
         {{ $t("lang-9801cd40-7281-47f7-8478-6731dc9d8388") }}
       </STabItem>
     </STabs>
 
     <STable
       :headers="headers"
-      :data="tableData"
+      :data="orderStore.orders"
       totalItems="50"
       itemsPerPage="5"
       paginateRange="2"
       @onSelectPage="onChangePage"
     >
-      <template v-slot:status="{ item }">
+      <!-- <template v-slot:status="{ item }">
         <SBadge :content="item.status.name" :color="item.status.color" />
-      </template>
+      </template> -->
       <template v-slot>
         <router-link to="/orders">
           {{ $t("lang-23981bea-cba2-425d-a435-41ae4a591794") }}
@@ -42,49 +42,36 @@
 
 <script lang="ts" setup>
 import { STable, STabs, STabItem, SBadge } from "@tumarsoft/ogogo-ui";
-import { ref, reactive } from "vue";
+import { ref, onMounted } from "vue";
 import { FilterSearch } from "@/shared/ui/components/filter-search";
+import { useOrderStore } from "../store/order.store";
 
-const tab = ref("one");
-
-const headers = reactive([
-  { title: "Товар", key: "product" },
-  { title: "Продавец", key: "name" },
+const headers = ref([
+  { title: "Номер заказа", key: "orderNumber" },
   { title: "Статус", key: "status" },
-  { title: "Дата", key: "date" },
+  { title: "Дата оформления", key: "registrationDate" },
+  { title: "Оплата", key: "paymentType" },
+  { title: "Сумма заказа", key: "price" },
+  { title: "Кол-во товара", key: "count" },
   { title: "Действия", key: "action" },
 ]);
-
-const tableData = reactive([
-  {
-    id: 1,
-    product: "Смартфон Apple iPhone 15 Pro 256Gb Natural Titanium 2 SIM HK/CN",
-    status: { name: "На проверке", color: "orange" },
-    name: "Планета электроники",
-    date: "01.01.24",
-  },
-  {
-    id: 2,
-    product: "Смартфон Apple iPhone 15 Pro 256Gb Natural Titanium 2 SIM HK/CN",
-    status: { name: "Модерируется", color: "blue" },
-    name: "Gadget.kg",
-    date: "02.02.24",
-  },
-  {
-    id: 3,
-    product: "Смартфон Apple iPhone 15 Pro 256Gb Natural Titanium 2 SIM HK/CN",
-    status: { name: "Проверен", color: "green" },
-    name: "Моби-маркет",
-    date: "03.03.24",
-  },
-  {
-    id: 4,
-    product: "Смартфон Apple iPhone 15 Pro 256Gb Natural Titanium 2 SIM HK/CN",
-    status: { name: "Отклонен", color: "rose" },
-    name: "Софтек",
-    date: "04.04.24",
-  },
+const statuses = ref([
+  { id: 0, name: "Все", color: "green" },
+  { id: 14400, name: "Активные", color: "green" },
+  { id: 14401, name: "Отмененные", color: "green" },
+  { id: 14402, name: "Архив", color: "green" },
 ]);
+const orderStore = useOrderStore();
+const tab = ref("one");
+// const tableData = ref([]);
+
+onMounted(() => {
+  fetchOrders();
+});
+
+const fetchOrders = () => {
+  orderStore.getAllOrders({});
+};
 
 const selectTab = (value: any) => {
   tab.value = value;
