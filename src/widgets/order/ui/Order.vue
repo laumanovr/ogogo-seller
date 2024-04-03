94b25f0c5fbc0f310a06ca4a31b8e73291bb61f2
Ramazan <laumanov.r@gmail.com>
2024-04-03 15:29:57 +0600

    add pagination in orders

--- src/widgets/order/ui/Order.vue
+++ src/widgets/order/ui/Order.vue
@@ -3,53 +3,56 @@
     <div class="head-title">
       {{ $t("lang-6f853bc8-3f4e-44d2-9770-49d98347fc6b") }}
     </div>
-    <FilterSearch />
+    <template v-if="isOrderExist">
+      <FilterSearch />
 
-    <STabs :tab-mode="'filter-tabs'" class="mb-20">
-      <STabItem value="14400" :active-tab="tab" @changeTab="selectTab">
-        {{ $t("lang-dd1a9bc2-31f2-4197-9a7d-01a23229ea82") }}
-      </STabItem>
-      <STabItem value="0" :active-tab="tab" @changeTab="selectTab">
-        {{ $t("lang-7db32df9-54d2-4561-ba8b-c43073ee42e9") }}
-      </STabItem>
-      <STabItem value="14401" :active-tab="tab" @changeTab="selectTab">
-        {{ $t("lang-b5ea90d6-b263-4058-b295-b41b2d72c542") }}
-      </STabItem>
-      <STabItem value="14402" :active-tab="tab" @changeTab="selectTab">
-        {{ $t("lang-9801cd40-7281-47f7-8478-6731dc9d8388") }}
-      </STabItem>
-    </STabs>
+      <STabs :tab-mode="'filter-tabs'" class="mb-20">
+        <STabItem value="14400" :active-tab="tab" @changeTab="selectTab">
+          {{ $t("lang-dd1a9bc2-31f2-4197-9a7d-01a23229ea82") }}
+        </STabItem>
+        <STabItem value="0" :active-tab="tab" @changeTab="selectTab">
+          {{ $t("lang-7db32df9-54d2-4561-ba8b-c43073ee42e9") }}
+        </STabItem>
+        <STabItem value="14401" :active-tab="tab" @changeTab="selectTab">
+          {{ $t("lang-b5ea90d6-b263-4058-b295-b41b2d72c542") }}
+        </STabItem>
+        <STabItem value="14402" :active-tab="tab" @changeTab="selectTab">
+          {{ $t("lang-9801cd40-7281-47f7-8478-6731dc9d8388") }}
+        </STabItem>
+      </STabs>
 
-    <STable
-      :headers="headers"
-      :data="orderStore.orders"
-      totalItems="50"
-      itemsPerPage="5"
-      paginateRange="2"
-      @onSelectPage="onChangePage"
-    >
-      <template v-slot:price="{ item }">
-        <div>{{ item.price }} сом</div>
-        <div class="price-usd">{{ item.priceUsd }} $</div>
-      </template>
-      <template v-slot:status="{ item }">
-        <SBadge
-          :content="showStatusData(item, 'name')"
-          :color="showStatusData(item, 'color')"
-        />
-      </template>
-      <template v-slot:registrationDate="{ item }">
-        {{ showPurchaseDate(item) }}
-      </template>
-      <template v-slot:paymentType="{ item }">
-        {{ showPaymentType(item) }}
-      </template>
-      <template v-slot:action="{ item }">
-        <router-link to="/orders">
-          {{ $t("lang-23981bea-cba2-425d-a435-41ae4a591794") }}
-        </router-link>
-      </template>
-    </STable>
+      <STable
+        :headers="headers"
+        :data="orderStore.orders"
+        :totalItems="totalItems"
+        itemsPerPage="10"
+        paginateRange="2"
+        @onSelectPage="onChangePage"
+        v-if="hasOrders"
+      >
+        <template v-slot:price="{ item }">
+          <div>{{ item.price }} сом</div>
+          <div class="price-usd">{{ item.priceUsd }} $</div>
+        </template>
+        <template v-slot:status="{ item }">
+          <SBadge
+            :content="showStatusData(item, 'name')"
+            :color="showStatusData(item, 'color')"
+          />
+        </template>
+        <template v-slot:registrationDate="{ item }">
+          {{ showPurchaseDate(item) }}
+        </template>
+        <template v-slot:paymentType="{ item }">
+          {{ showPaymentType(item) }}
+        </template>
+        <template v-slot:action="{ item }">
+          <router-link to="/orders">
+            {{ $t("lang-23981bea-cba2-425d-a435-41ae4a591794") }}
+          </router-link>
+        </template>
+      </STable>
+    </template>
   </div>
 </template>
 
@@ -76,6 +79,9 @@ const statuses = ref([
 ]);
 const orderStore = useOrderStore();
 const tab = ref("one");
+const isOrderExist = ref(false);
+const hasOrders = ref(false);
+const totalItems = ref(0);
 
 const currentStatus = computed(() =>
   Number(tab.value) ? { queryParams: { statuses: [Number(tab.value)] } } : {}
@@ -86,7 +92,11 @@ onMounted(() => {
 });
 
 const fetchOrders = (filterObj = {}) => {
-  orderStore.getAllOrders({ ...filterObj });
+  orderStore.getAllOrders({ ...filterObj }).then((response) => {
+    isOrderExist.value = Boolean(response.totalPages);
+    hasOrders.value = Boolean(response.items.length);
+    totalItems.value = response.totalCount;
+  });
 };
 
 const selectTab = (value: any) => {
@@ -109,8 +119,8 @@ const showPaymentType = (item: any) => {
   return item.paymentType === 14500 ? "Кредит" : "Оплачено";
 };
 
-const onChangePage = (page: any) => {
-  console.log(page);
+const onChangePage = (page: number) => {
+  fetchOrders({ ...currentStatus.value, pageIndex: page });
 };
 </script>
 
