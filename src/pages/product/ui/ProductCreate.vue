e2dc8cf76f33b4374361570e1fbbbb843c87f71a
Ramazan <laumanov.r@gmail.com>
2024-04-24 18:18:21 +0600

    add product-create page

---
+++ src/pages/product/ui/ProductCreate.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="product-create-container">
+    <ProductCreate />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ProductCreate } from "@/widgets/product-create";
+</script>
