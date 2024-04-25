271d66d587d15e07523a903fb7ba05e85743bebf
Ramazan <laumanov.r@gmail.com>
2024-04-25 17:03:44 +0600

    add button for adding template

--- src/widgets/product-create/ui/ProductCreate.vue
+++ src/widgets/product-create/ui/ProductCreate.vue
@@ -46,24 +46,29 @@
             width="100%"
             class="mt-12 mb-12"
           />
-          <div class="template" v-for="i in 3" :key="i">
-            <SCheckbox>
-              <img
-                src="../../../app/assets/img/iphone.jpg"
-                alt="img"
-                class="sample__img"
-              />
-              <div class="template__info">
-                <div class="template__name">
-                  Смартфон Apple iPhone 15 Pro 256Gb Natural Titanium 2 SIM
-                  HK/CN
+          <div class="template-container">
+            <div class="template" v-for="i in 10" :key="i">
+              <SCheckbox>
+                <img
+                  src="../../../app/assets/img/iphone.jpg"
+                  alt="img"
+                  class="sample__img"
+                />
+                <div class="template__info">
+                  <div class="template__name">
+                    Смартфон Apple iPhone 15 Pro 256Gb Natural Titanium 2 SIM
+                    HK/CN
+                  </div>
+                  <div class="template__desc">
+                    15 Pro • Natural Titanium • 128 ГБ
+                  </div>
                 </div>
-                <div class="template__desc">
-                  15 Pro • Natural Titanium • 128 ГБ
-                </div>
-              </div>
-            </SCheckbox>
+              </SCheckbox>
+            </div>
           </div>
+          <SButton color="violet" size="large" class="w-p-100 mt-24">
+            Добавить шаблоны
+          </SButton>
         </STabWindow>
       </div>
       <div class="form-container"></div>
@@ -151,25 +156,36 @@ const selectSample = (index: number) => {
         }
       }
     }
-    .template {
-      margin-bottom: 15px;
-      &__name {
-        font-size: 14px;
-        font-weight: 600;
-        max-height: 50px;
-        overflow-y: hidden;
+    .template-container {
+      max-height: 500px;
+      overflow-y: auto;
+      &::-webkit-scrollbar {
+        width: 8px;
       }
-      &__desc {
-        font-size: 12px;
-        font-weight: 500;
-        color: $gray-500;
-        margin-top: 8px;
+      &::-webkit-scrollbar-thumb {
+        background-color: $gray-300;
+        border-radius: 4px;
       }
-      .checkbox-container {
-        align-items: center;
-      }
-      .checkbox-title {
-        display: flex;
+      .template {
+        margin-bottom: 15px;
+        &__name {
+          font-size: 14px;
+          font-weight: 600;
+          max-height: 50px;
+          overflow-y: hidden;
+        }
+        &__desc {
+          font-size: 12px;
+          font-weight: 500;
+          color: $gray-500;
+          margin-top: 8px;
+        }
+        .checkbox-container {
+          align-items: center;
+        }
+        .checkbox-title {
+          display: flex;
+        }
       }
     }
   }
