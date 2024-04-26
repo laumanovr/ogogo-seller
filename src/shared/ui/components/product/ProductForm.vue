8ff79d899e92c884f77e75a3addfba535dea6b43
Ramazan <laumanov.r@gmail.com>
2024-04-26 17:53:06 +0600

    preparing product-form

--- src/shared/ui/components/product/ProductForm.vue
+++ src/shared/ui/components/product/ProductForm.vue
@@ -1,14 +1,54 @@
 <template>
-  <div class="product-form-container">qwerrtty</div>
+  <div class="product-form-container">
+    <div class="head-title md">Главное о товаре</div>
+    <div>
+      <SInput label="Наименование товара *" width="100%" />
+    </div>
+    <div class="d-flex items-end mt-16">
+      <SInput label="Ваш артикул" width="85%" />
+      <SButton color="gray" class="ml-12">Сгенерировать</SButton>
+    </div>
+    <div class="mt-16">
+      <SInput label="Артикул производителя" width="100%" />
+    </div>
+    <div class="mt-16 w-p-100">
+      <STextArea label="Описание товара" width="100%" />
+    </div>
+    <div class="d-flex items-center justify-between mt-40 mb-30">
+      <div class="head-title md">Цена</div>
+      <div class="d-flex">
+        <SRadioButton class="mr-24">В сомах</SRadioButton>
+        <SRadioButton>В долларах</SRadioButton>
+      </div>
+    </div>
+    <div class="d-flex justify-between">
+      <SInput label="Цена в сомах" width="32%" />
+      <SInput label="Скидка в %" width="32%" />
+      <SInput label="Цена со скидкой" width="32%" />
+    </div>
+    <div class="mt-16 mb-40">
+      <SInput label="Наличие (кол-во) *" width="100%" />
+    </div>
+    <div class="head-title md">Фото</div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {
+  SInput,
+  SButton,
+  STextArea,
+  SSelect,
+  SRadioButton,
+} from "@tumarsoft/ogogo-ui";
+</script>
 
 <style lang="scss">
 @import "../../../../app/style/colors.scss";
 .product-form-container {
   width: 68%;
-  height: 200px;
-  border: 1px solid;
+  .head-title {
+    margin: 0 0 25px;
+  }
 }
 </style>
