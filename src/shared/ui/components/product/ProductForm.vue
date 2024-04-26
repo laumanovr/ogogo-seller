51b8f304f10a606841c8d51834e93f472d0086b4
Ramazan <laumanov.r@gmail.com>
2024-04-26 14:57:42 +0600

    create separate product-form component

---
+++ src/shared/ui/components/product/ProductForm.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="product-form-container">qwerrtty</div>
+</template>
+
+<script lang="ts" setup></script>
+
+<style lang="scss">
+@import "../../../../app/style/colors.scss";
+.product-form-container {
+  width: 68%;
+  height: 200px;
+  border: 1px solid;
+}
+</style>
