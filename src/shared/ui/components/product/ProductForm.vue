1e74af4ca468972b59d5cc6bacd68b366880988e
Ramazan <laumanov.r@gmail.com>
2024-05-02 16:59:40 +0600

    prepared fields for payload

--- src/shared/ui/components/product/ProductForm.vue
+++ src/shared/ui/components/product/ProductForm.vue
@@ -2,7 +2,11 @@
   <div class="product-form-container">
     <div class="head-title md">Главное о товаре</div>
     <div>
-      <SInput label="Наименование товара *" width="100%" />
+      <SInput
+        label="Наименование товара *"
+        width="100%"
+        v-model="productObj.productName"
+      />
     </div>
     <div class="d-flex items-end mt-16">
       <SInput
@@ -10,30 +14,45 @@
         width="85%"
         class="article"
         disabled
-        v-model="productObj.article"
+        v-model="productObj.toArticle"
       />
       <SButton color="gray" class="ml-12">Сгенерировать</SButton>
     </div>
-    <div class="mt-16">
-      <SInput label="Артикул производителя" width="100%" />
-    </div>
+    <!-- <div class="mt-16">
+      <SInput label="Артикул производителя" width="100%"  />
+    </div> -->
     <div class="mt-16 w-p-100">
-      <STextArea label="Описание товара" width="100%" />
+      <STextArea
+        label="Описание товара"
+        width="100%"
+        v-model="productObj.description"
+      />
     </div>
     <div class="d-flex items-center justify-between mt-40 mb-30">
       <div class="head-title md">Цена</div>
       <div class="d-flex">
-        <SRadioButton class="mr-24 radio-btn">В сомах</SRadioButton>
-        <SRadioButton class="radio-btn">В долларах</SRadioButton>
+        <SRadioButton
+          class="mr-24 radio-btn"
+          @onChange="onSelectPriceType(14600)"
+        >
+          В сомах
+        </SRadioButton>
+        <SRadioButton class="radio-btn" @onChange="onSelectPriceType(14601)">
+          В долларах
+        </SRadioButton>
       </div>
     </div>
     <div class="d-flex justify-between">
-      <SInput label="Цена в сомах" width="32%" />
-      <SInput label="Скидка в %" width="32%" />
-      <SInput label="Цена со скидкой" width="32%" />
+      <SInput label="Цена" width="32%" v-model="productObj.price" />
+      <SInput label="Скидка в %" width="32%" v-model="productObj.discount" />
+      <SInput label="Цена со скидкой" width="32%" v-model="priceWithDiscount" />
     </div>
     <div class="mt-16 mb-40">
-      <SInput label="Наличие (кол-во) *" width="100%" />
+      <SInput
+        label="Наличие (кол-во) *"
+        width="100%"
+        v-model="productObj.countOfProduct"
+      />
     </div>
     <div class="content-block">
       <div class="head-title md">Фото</div>
@@ -86,7 +105,9 @@
       </div>
     </div>
     <div class="d-flex justify-end mt-60">
-      <SButton color="violet" size="large">Опубликовать</SButton>
+      <SButton color="violet" size="large" @click="submitProduct">
+        Опубликовать
+      </SButton>
     </div>
   </div>
 </template>
@@ -103,9 +124,24 @@ import {
 import { ref } from "vue";
 
 const productObj = ref({
-  article: "article77",
+  toArticle: "",
+  productName: "",
+  description: "",
+  categoryId: "",
+  price: 0,
+  discount: 0,
+  organizationId: "",
+  productType: 0,
+  productPriceType: 0,
   photos: [],
+  videos: [],
+  templateId: "",
+  countOfProduct: 0,
+  isSaveAsDraft: false,
+  properties: {},
 });
+
+const priceWithDiscount = ref("");
 
 const convertToBase64 = (file: File) => {
   return new Promise((resolve, reject) => {
@@ -138,6 +174,14 @@ const deleteImage = (imgUrl: string) => {
 
 const onSelectVideo = (e: Event) => {
   console.log(e);
+};
+
+const onSelectPriceType = (priceType: number) => {
+  productObj.value.productPriceType = priceType;
+};
+
+const submitProduct = () => {
+  console.log(productObj.value);
 };
 </script>
 
