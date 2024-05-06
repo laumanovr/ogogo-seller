cb8051c0f6a0fae91857d90c8d9a49338366d847
Ramazan <laumanov.r@gmail.com>
2024-05-06 17:17:06 +0600

    create category-store

---
+++ src/entities/category/store/category.store.ts
@@ -0,0 +1,36 @@
+import { CategoryApiResponse } from "../api/category-api.types";
+import { defineStore } from "pinia";
+import { useAlertStore } from "@/shared/store/alert";
+import { useLoaderStore } from "@/shared/store/loader";
+import { ICategoryState } from "./category-store.types";
+import { CategoryApi } from "../api/category.api";
+
+const loaderStore = useLoaderStore();
+const alertStore = useAlertStore();
+const categoryApi = new CategoryApi();
+
+export const useCategoryStore = defineStore("categoryStore", {
+  state: (): Partial<ICategoryState> => ({
+    categories: [],
+  }),
+  getters: {},
+  actions: {
+    getAllCategories(): Promise<CategoryApiResponse> {
+      return new Promise((resolve, reject) => {
+        loaderStore.setLoaderState(true);
+        categoryApi
+          .getCategories()
+          .then((response) => {
+            this.categories = response.result;
+            loaderStore.setLoaderState(false);
+            resolve(response);
+          })
+          .catch((err) => {
+            alertStore.showError(err.message);
+            loaderStore.setLoaderState(false);
+            reject(err);
+          });
+      });
+    },
+  },
+});
