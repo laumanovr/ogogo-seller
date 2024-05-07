b99006cdb20d17ee652529a5f5a221a9488b3142
Ramazan <laumanov.r@gmail.com>
2024-05-07 10:18:50 +0600

    moved category-types to category-api-types file

--- src/entities/category/api/category-api.types.ts
+++ src/entities/category/api/category-api.types.ts
@@ -1,8 +1,13 @@
-import {
-  //   BasePageParams,
-  ResponseWithStatus,
-  //   WithResultPagination,
-} from "@/shared/api/index.types";
-import { CategoryEntity } from "../model/types";
+import { ResponseWithStatus } from "@/shared/api/index.types";
+import { CategoryEntity, CategoryPagedEntity } from "../model/types";
 
 export type CategoryApiResponse = ResponseWithStatus<CategoryEntity[]>;
+
+export interface CategoryPagedPayload {
+  search?: string;
+  pageSize?: number;
+}
+
+export type CategoryPagedResponse = {
+  items: Array<CategoryPagedEntity>;
+};
