8eeda3802c8b26719d580e6a6f7566ef981499a8
Ramazan <laumanov.r@gmail.com>
2024-05-15 11:40:03 +0600

    preparing form-validation

--- src/shared/ui/components/product/ProductForm.vue
+++ src/shared/ui/components/product/ProductForm.vue
@@ -1,147 +1,155 @@
 <template>
   <div class="product-form-container">
     <div class="head-title md">Главное о товаре</div>
-    <div>
-      <SInput
-        label="Наименование товара *"
-        width="100%"
-        v-model="productStore.productTemplate.productName"
-      />
-    </div>
-    <div class="d-flex items-end mt-16">
-      <SInput
-        label="Ваш артикул"
-        width="85%"
-        class="article"
-        disabled
-        v-model="productStore.productTemplate.toArticle"
-      />
-      <SButton color="gray" class="ml-12">Сгенерировать</SButton>
-    </div>
-    <!-- <div class="mt-16">
-      <SInput label="Артикул производителя" width="100%"  />
-    </div> -->
-    <div class="mt-16 w-p-100">
-      <STextArea
-        label="Описание товара"
-        width="100%"
-        v-model="productStore.productTemplate.description"
-      />
-    </div>
-    <div class="d-flex items-center justify-between mt-40 mb-30">
-      <div class="head-title md">Цена</div>
-      <div class="d-flex">
-        <SRadioButton
-          class="mr-24 radio-btn"
-          @onChange="onSelectPriceType(14600)"
-        >
-          В сомах
-        </SRadioButton>
-        <SRadioButton class="radio-btn" @onChange="onSelectPriceType(14601)">
-          В долларах
-        </SRadioButton>
+    <SForm ref="productForm">
+      <div>
+        <SInput
+          label="Наименование товара"
+          width="100%"
+          :rules="requiredField"
+          v-model="productStore.productTemplate.productName"
+        />
       </div>
-    </div>
-    <div class="d-flex justify-between">
-      <SInput
-        type="number"
-        label="Цена"
-        width="32%"
-        v-model="productStore.productTemplate.price"
-      />
-      <SInput
-        type="number"
-        label="Скидка в %"
-        width="32%"
-        v-model="productStore.productTemplate.discount"
-        @input="countPriceDiscount"
-      />
-      <SInput
-        type="number"
-        label="Цена со скидкой"
-        width="32%"
-        v-model="priceWithDiscount"
-      />
-    </div>
-    <div class="mt-16 mb-40">
-      <SInput
-        label="Наличие (кол-во) *"
-        width="100%"
-        type="number"
-        v-model="productStore.productTemplate.countOfProduct"
-      />
-    </div>
-    <div class="content-block">
-      <div class="head-title md">Фото</div>
-      <p class="content-hint">
-        Добавьте изображение товара. Изображение максимум 2000 х 2000 px, в
-        формате PNG или JPEG. <br />
-        Размер файла – не более 15 МБ.
-      </p>
-      <div class="d-flex flex-wrap">
-        <div class="photo" v-for="image in productImages" :key="image">
-          <img :src="image" alt="img" />
-          <SIconRender
-            name="CloseRoundIcon"
-            color="grey"
-            class="close-icon"
-            @click="deleteImage(image)"
-          />
-        </div>
-        <label for="photo" class="add-content">
-          <input type="file" id="photo" @change="onSelectPhoto" />
-          <span>+</span>
-        </label>
+      <div class="d-flex items-end mt-16">
+        <SInput
+          label="Ваш артикул"
+          width="85%"
+          class="article"
+          :rules="requiredField"
+          v-model="productStore.productTemplate.toArticle"
+        />
+        <SButton color="gray" class="ml-12">Сгенерировать</SButton>
       </div>
-    </div>
-    <div class="content-block mt-40">
-      <div class="head-title md">Видео</div>
-      <p class="content-hint">
-        На карточке может быть только одно видео – покажем его первым. Принимаем
-        качественные ролики <br />
-        не больше 30 МБ и не длинее 5 минут.
-      </p>
-      <div class="d-flex">
-        <video class="video" controls width="180" height="180" :key="videoKey">
-          <source :src="videoUrl" type="video/mp4" />
-        </video>
-        <label for="video" class="add-content ml-10">
-          <input type="file" id="video" @change="onSelectVideo" />
-          <span>+</span>
-        </label>
+      <div class="mt-16 w-p-100">
+        <STextArea
+          label="Описание товара"
+          width="100%"
+          v-model="productStore.productTemplate.description"
+        />
       </div>
-    </div>
-    <div class="content-block mt-40">
-      <div class="head-title md">Характеристики</div>
-      <p class="content-hint">
-        Это характеристики товара, которые могут быть выбраны покупателем при
-        покупке. Например, цвет, <br />
-        материал, объем и другие характеристики, которые могут варьироваться для
-        одного и того же товара
-      </p>
-      <div class="d-flex justify-between flex-wrap">
-        <div
-          class="select-item mb-16"
-          v-for="property in properties"
-          :key="property.propertyId"
-        >
-          <SSelect
-            :label="showPropertyName(property)"
-            :items="property.allowedValues"
-            showValue="propertyValueText"
-            getValue="propertyValueId"
-            v-model="property.selectedValueId"
-            class="w-p-100"
-            @onChange="onSelectProperty(property)"
-          />
+      <div class="d-flex items-center justify-between mt-40 mb-30">
+        <div class="head-title md">Цена</div>
+        <div class="d-flex">
+          <SRadioButton
+            class="mr-24 radio-btn"
+            isSelected
+            @onChange="onSelectPriceType(14600)"
+          >
+            В сомах
+          </SRadioButton>
+          <SRadioButton class="radio-btn" @onChange="onSelectPriceType(14601)">
+            В долларах
+          </SRadioButton>
         </div>
       </div>
-    </div>
-    <div class="d-flex justify-end mt-60">
-      <SButton color="violet" size="large" @click="submitProduct">
-        Опубликовать
-      </SButton>
-    </div>
+      <div class="d-flex justify-between">
+        <SInput
+          label="Цена"
+          width="32%"
+          :rules="requiredField"
+          v-model="productStore.productTemplate.price"
+        />
+        <SInput
+          type="number"
+          label="Скидка в %"
+          width="32%"
+          v-model="productStore.productTemplate.discount"
+          @input="countPriceDiscount"
+        />
+        <SInput
+          type="number"
+          label="Цена со скидкой"
+          width="32%"
+          v-model="priceWithDiscount"
+        />
+      </div>
+      <div class="mt-16 mb-40">
+        <SInput
+          label="Наличие (кол-во) *"
+          width="100%"
+          :rules="requiredField"
+          v-model="productStore.productTemplate.countOfProduct"
+        />
+      </div>
+      <div class="content-block">
+        <div class="head-title md">Фото</div>
+        <p class="content-hint">
+          Добавьте изображение товара. Изображение максимум 2000 х 2000 px, в
+          формате PNG или JPEG. <br />
+          Размер файла – не более 15 МБ.
+        </p>
+        <div class="d-flex flex-wrap">
+          <div class="photo" v-for="image in productImages" :key="image">
+            <img :src="image" alt="img" />
+            <SIconRender
+              name="CloseRoundIcon"
+              color="grey"
+              class="close-icon"
+              @click="deleteImage(image)"
+            />
+          </div>
+          <label for="photo" class="add-content">
+            <input type="file" id="photo" @change="onSelectPhoto" />
+            <span>+</span>
+          </label>
+        </div>
+      </div>
+      <div class="content-block mt-40">
+        <div class="head-title md">Видео</div>
+        <p class="content-hint">
+          На карточке может быть только одно видео – покажем его первым.
+          Принимаем качественные ролики <br />
+          не больше 30 МБ и не длинее 5 минут.
+        </p>
+        <div class="d-flex">
+          <video
+            class="video"
+            controls
+            width="180"
+            height="180"
+            :key="videoKey"
+          >
+            <source :src="videoUrl" type="video/mp4" />
+          </video>
+          <label for="video" class="add-content ml-10">
+            <input type="file" id="video" @change="onSelectVideo" />
+            <span>+</span>
+          </label>
+        </div>
+      </div>
+      <div class="content-block mt-40">
+        <div class="head-title md">Характеристики</div>
+        <p class="content-hint">
+          Это характеристики товара, которые могут быть выбраны покупателем при
+          покупке. Например, цвет, <br />
+          материал, объем и другие характеристики, которые могут варьироваться
+          для одного и того же товара
+        </p>
+        <div class="d-flex justify-between flex-wrap">
+          <div
+            class="select-item mb-16"
+            v-for="property in properties"
+            :key="property.propertyId"
+          >
+            <SSelect
+              class="w-p-100"
+              :label="showPropertyName(property)"
+              :items="property.allowedValues"
+              :rules="requiredField"
+              showValue="propertyValueText"
+              getValue="propertyValueId"
+              v-model="property.selectedValueId"
+              @onChange="onSelectProperty(property)"
+            />
+          </div>
+        </div>
+      </div>
+      <div class="d-flex justify-end mt-60">
+        <SButton color="violet" size="large" @click="submitProduct">
+          Опубликовать
+        </SButton>
+      </div>
+    </SForm>
   </div>
 </template>
 
@@ -153,11 +161,13 @@ import {
   SSelect,
   SRadioButton,
   SIconRender,
+  SForm,
 } from "@tumarsoft/ogogo-ui";
 import { ref, onMounted } from "vue";
 import { useProductStore } from "@/entities/products/store/product.store";
 import { useCategoryStore } from "@/entities/category/store/category.store";
 import { useProfileStore } from "@/widgets/profile/store/profile.store";
+import { requiredField } from "@/shared/lib/utils/rules";
 import { useRoute } from "vue-router";
 import { useRouter } from "vue-router";
 
@@ -173,8 +183,10 @@ const properties = ref([]);
 const selectedCategoryId = route.query.id;
 const propertyObject = ref<any>({});
 const productImages = ref([]);
+const productForm = ref(null);
 
 onMounted(() => {
+  onSelectPriceType(14600);
   categoryStore
     .getCategoryWithPropertiesById(selectedCategoryId as string)
     .then((response) => {
@@ -237,14 +249,16 @@ const onSelectProperty = (property: any) => {
 };
 
 const submitProduct = () => {
+  console.log("isValid:", productForm.value.validateForm());
   productStore.productTemplate.organizationId =
     profileStore.currentUser.organizationId;
   productStore.productTemplate.productType = 14701;
   productStore.productTemplate.categoryId = selectedCategoryId as string;
   productStore.productTemplate.properties = propertyObject.value;
-  productStore.createProduct(productStore.productTemplate).then(() => {
-    router.push("/products");
-  });
+  console.log(productStore.productTemplate);
+  // productStore.createProduct(productStore.productTemplate).then(() => {
+  //   router.push("/products");
+  // });
 };
 </script>
 
