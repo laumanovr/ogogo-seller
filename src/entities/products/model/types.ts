6b87790eaa4ed6c07b462affb3d436e385c8459e
Ramazan <laumanov.r@gmail.com>
2024-05-15 17:46:11 +0600

    modified types

--- src/entities/products/model/types.ts
+++ src/entities/products/model/types.ts
@@ -26,7 +26,7 @@ export interface ProductTemplateEntity {
   productName: string;
   description: string;
   categoryId: string;
-  price: string;
+  price: number | string;
   discount: number;
   organizationId: string;
   productType: number;
@@ -34,7 +34,7 @@ export interface ProductTemplateEntity {
   photos: string[];
   videos: string[];
   templateId: string;
-  countOfProduct: string;
+  countOfProduct: number | string;
   isSaveAsDraft: Boolean;
   properties: Object;
 }
