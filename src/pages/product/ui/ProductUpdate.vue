b4fdcaeadbd0518c1adabcdc1b653b602b3813c0
Ramazan <laumanov.r@gmail.com>
2024-05-16 18:49:54 +0600

    create page for product-update

---
+++ src/pages/product/ui/ProductUpdate.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="product-update-container">
+    <ProductUpdate />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ProductUpdate } from "@/widgets/product-update";
+</script>
