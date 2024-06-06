92c6a1fca822a18ef2a01a364582b16cf35e1ff6
Ramazan <laumanov.r@gmail.com>
2024-06-06 15:06:46 +0600

    remade required-field rule

--- src/shared/lib/utils/rules.ts
+++ src/shared/lib/utils/rules.ts
@@ -1,6 +1,3 @@
-export const requiredField = [
-  {
-    validate: (value: any) => value || value.length >= 1,
-    message: "Обязательное поле",
-  },
-];
+export const requiredField = (value: any) => {
+  return (value && value.length >= 1) || "Обязательное поле";
+};
