83f43c56eaf01f644538bc7743257fca5aa30c1b
Ramazan <laumanov.r@gmail.com>
2024-06-11 16:09:21 +0600

    modified required rule

--- src/shared/lib/utils/rules.ts
+++ src/shared/lib/utils/rules.ts
@@ -1,3 +1,3 @@
 export const requiredField = (value: any) => {
-  return (value && value.length >= 1) || "Обязательное поле";
+  return (value && value.length >= 1) || Number(value) || "Обязательное поле";
 };
