e1b351b4138b0e4825dbbdab4545855e72cd509d
Ramazan <laumanov.r@gmail.com>
2024-07-15 14:31:36 +0600

    modified router-interceptor

--- src/app/router/index.ts
+++ src/app/router/index.ts
@@ -1,6 +1,8 @@
 import { AuthMiddleware } from "@/app/router/middlware/auth";
 import { AccessRequestMiddleware } from "@/app/router/middlware/access-request";
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
+import { getItem } from "@/shared/lib/utils/persistanceStorage";
+import { RouteLocationNormalized } from "vue-router";
 import loginRoutes from "@/pages/login/router";
 import passwordReset from "@/pages/password-reset/router";
 import sellerRegistration from "@/pages/seller-registration/router";
@@ -24,11 +26,34 @@ const router = createRouter({
   routes,
 });
 
-
 const middlewares = [new AuthMiddleware(), new AccessRequestMiddleware()];
 
-router.beforeEach(() => {
-  middlewares;
-});
+router.beforeEach(
+  (
+    to: RouteLocationNormalized,
+    // @ts-ignore
+    from: RouteLocationNormalized,
+    next: Function
+  ) => {
+    middlewares;
+    const isAuthenticated = Boolean(getItem("sessionId")),
+      requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+    if (!isAuthenticated) {
+      if (
+        requiresAuth &&
+        to.name !== "password-reset" &&
+        to.name !== "seller-registration"
+      ) {
+        return next({ name: "login" });
+      }
+      next();
+    } else if (to.name === "login" && !isAuthenticated) {
+      next({ name: "profile" });
+    } else {
+      next();
+    }
+  }
+);
 
 export default router;
--- src/main.ts
+++ src/main.ts
@@ -7,42 +7,8 @@ import interceptors from "@/shared/lib/plugins/axios";
 import "virtual:uno.css";
 import i18n from "@/shared/lib/plugins/i18n";
 import { vMaska } from "maska";
-import { getItem } from "./shared/lib/utils/persistanceStorage";
-import { RouteLocationNormalized } from "vue-router";
 
 interceptors();
-
-// TODO: extract router interceptor setup to its own file
-router.beforeEach(
-  (
-    to: RouteLocationNormalized,
-    // @ts-ignore
-    from: RouteLocationNormalized,
-    next: Function
-  ) => {
-    const isAuthenticated = Boolean(getItem("sessionId")),
-      requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
-
-    if (!isAuthenticated) {
-      // TODO: change comparison from path to name for route "to"
-      if (
-        requiresAuth &&
-        to.path !== "/password-reset" &&
-        to.path !== "/seller-registration"
-      ) {
-        // TODO: change path to name
-        return next("/");
-      }
-      next();
-      // TODO: change path to name
-    } else if (to.path === "/" && !isAuthenticated) {
-      // TODO: change path to name
-      next("/profile");
-    } else {
-      next();
-    }
-  }
-);
 
 const app = createApp(App);
 
