13f87852960a98636bfc7332c9dd15cf51dd4ac1
Ramazan <laumanov.r@gmail.com>
2024-07-16 18:02:53 +0600

    add enums

--- src/shared/lib/utils/enums.ts
+++ src/shared/lib/utils/enums.ts
@@ -4,3 +4,15 @@ export enum MenuItems {
   Notifications = "notifications",
   Cabinet = "profile",
 }
+
+export enum ProductStatus {
+ALL = 0, 
+DRAFT = 14800,
+PUBLISHED = 14801, 
+AWAITING_MODERATION = 14802,
+AWAITING_APPROVE_TO = 14803, 
+APPROVED = 14804, 
+ARCHIVED = 14805, 
+REQUIRE_IMPROVEMENT = 14806, 
+BLOCKED = 14807, 
+}
