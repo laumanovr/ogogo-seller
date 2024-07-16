b08e987e0d78f90e969c3e3c72ef1af6ccb2daae
Ramazan <laumanov.r@gmail.com>
2024-07-16 11:35:59 +0600

    modified loader

--- src/shared/ui/components/Loader.vue
+++ src/shared/ui/components/Loader.vue
@@ -1,47 +1,26 @@
 <template>
-  <div class="loader-container">
+  <div>
+    <div class="loader-container s-bg-white"></div>
     <img src="@/shared/ui/assets/loader.gif" class="loader" />
   </div>
 </template>
 
-<!-- TODO: set scoped for this component -->
-<style lang="scss">
-@import "../../../app/style/colors.scss";
-
-// TODO: set bacground by class
+<style lang="scss" scoped>
 .loader-container {
   position: fixed;
-  z-index: 999;
-  background: rgba($white, 0.5);
+  opacity: 0.5;
   width: 100%;
   height: 100%;
   top: 0;
   left: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  img {
-    width: 200px;
-    height: 200px;
-  }
 }
-
-// TODO: extract spin animation to ui kit utility class
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-
-@-webkit-keyframes spin {
-  0% {
-    -webkit-transform: rotate(0deg);
-  }
-  100% {
-    -webkit-transform: rotate(360deg);
-  }
+.loader {
+  width: 200px;
+  height: 200px;
+  z-index: 99999;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
