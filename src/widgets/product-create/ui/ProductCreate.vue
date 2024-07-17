c673bbca03b7755f7351689f2513c9148dc31739
Ramazan <laumanov.r@gmail.com>
2024-07-17 16:04:31 +0600

    modified product-create

--- src/widgets/product-create/ui/ProductCreate.vue
+++ src/widgets/product-create/ui/ProductCreate.vue
@@ -1,19 +1,13 @@
 <template>
-  <!-- TODO: remove unused classes -->
-  <div class="product-create-content">
-    <!-- TODO: remove preset classes and use ui kit classes -->
-    <!-- TODO: set theme class at layout level -->
-    <div class="d-flex items-center light">
+  <div>
+    <div class="s-flex s-items-center">
       <SButton type="secondary" variant="outlined" @click="goBack">
         <SIconRender name="chevron-left" size="small" />
-        <!-- TODO: localize text -->
-        <span>Назад</span>
+        <span>{{ $t("lang-943d7231-c402-4b11-929c-b26a3ee10276") }}</span>
       </SButton>
-      <!-- TODO: use computed getter for categoryUrls -->
-      <Breadcrumbs :items="categoryStore.categoryUrls" class="s-ml-5" />
+      <Breadcrumbs :items="categoryUrls" class="s-ml-5" />
     </div>
-    <!-- TODO: remove preset classes and use ui kit classes -->
-    <div class="d-flex s-mt-5">
+    <div class="s-flex s-mt-5">
       <ProductTemplate />
       <ProductForm />
     </div>
@@ -22,27 +16,24 @@
 
 <script lang="ts" setup>
 import { SButton, SIconRender } from "@tumarsoft/ogogo-ui";
-// TODO: use absolute path. example: import ProductTemplate from "@/widgets/product-create/ui/ProductTemplate.vue";
-import ProductTemplate from "../../../shared/ui/components/product/ProductTemplate.vue";
-import ProductForm from "../../../shared/ui/components/product/ProductForm.vue";
+import ProductTemplate from "@/shared/ui/components/product/ProductTemplate.vue";
+import ProductForm from "@/shared/ui/components/product/ProductForm.vue";
 import Breadcrumbs from "@/shared/ui/components/Breadcrumbs/Breadcrumbs.vue";
-
 import { useCategoryStore } from "@/entities/category/store/category.store";
 import { useProductStore } from "@/entities/products/store/product.store";
 import { useRouter } from "vue-router";
-import { onBeforeMount } from "vue";
+import { onBeforeMount, computed } from "vue";
 
 const router = useRouter();
 const categoryStore = useCategoryStore();
 const productStore = useProductStore();
+const categoryUrls = computed(() => categoryStore.getCategoryUrls);
 
 onBeforeMount(() => {
-  // TODO: use actions and set value of reactive data there
-  productStore.productTemplate = { photos: [], videos: [] };
+  productStore.setEmpty();
 });
 
 const goBack = () => {
-  // TODO: use name for router params. example: router.push({ name: "products" });  
-  router.push("/products");
+  router.push({ name: "products" });
 };
 </script>
