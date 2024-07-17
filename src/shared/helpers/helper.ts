38c8f3253f0c16107ab6494c8781885a904a2b34
Ramazan <laumanov.r@gmail.com>
2024-07-17 15:32:32 +0600

    create helper file

---
+++ src/shared/helpers/helper.ts
@@ -0,0 +1,36 @@
+import i18n from "@/shared/lib/plugins/i18n";
+
+export const tableHeaders = [
+  {
+    title: i18n.global.t("lang-463282b8-512e-4eae-8cde-815a2678e07d"),
+    key: "productName",
+  },
+  {
+    title: i18n.global.t("lang-0d638a69-fc78-48bc-8b4f-58582c384a1d"),
+    key: "status",
+  },
+  {
+    title: i18n.global.t("lang-059d243d-445e-4100-ac56-a5e7910df626"),
+    key: "articleNumber",
+  },
+  {
+    title: i18n.global.t("lang-333319c2-2df4-4057-a56a-28ddd7a790a1"),
+    key: "price",
+  },
+  {
+    title: i18n.global.t("lang-1f6f2dca-070c-48bc-941f-e1300024ffbb"),
+    key: "discount",
+  },
+  {
+    title: i18n.global.t("lang-5eb99c46-ed5f-4a24-85ad-d551ad812256"),
+    key: "priceWithDiscount",
+  },
+  {
+    title: i18n.global.t("lang-a6dc23d1-d5cc-4c0a-8412-32f6ff24a2dd"),
+    key: "countOfProduct",
+  },
+  {
+    title: i18n.global.t("lang-0543ee76-db56-47ac-b2e9-3b96fc831a8c"),
+    key: "action",
+  },
+];
