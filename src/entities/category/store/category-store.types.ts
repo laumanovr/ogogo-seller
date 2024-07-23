c3ea88538019ad3685ef8d040c164e9dcf9d6f31
Ramazan <laumanov.r@gmail.com>
2024-07-23 12:29:26 +0600

    modified category-store-types

--- src/entities/category/store/category-store.types.ts
+++ src/entities/category/store/category-store.types.ts
@@ -3,5 +3,11 @@ import { CategoryEntity } from "../model/types";
 export interface ICategoryState {
   categories: CategoryEntity[];
   categoryUrls: object[];
-  pagedCategories: object[]
+  pagedCategories: object[];
+  categoryWithProperties: CategoryProperty[];
+}
+
+interface CategoryProperty {
+  key: string;
+  selectedValueId: string;
 }
