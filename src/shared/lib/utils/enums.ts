f6fcd6487bc0093b9a4b32747d32f01e82d65672
Ramazan <laumanov.r@gmail.com>
2024-07-23 12:30:13 +0600

    modifed enum

--- src/shared/lib/utils/enums.ts
+++ src/shared/lib/utils/enums.ts
@@ -6,17 +6,27 @@ export enum MenuItems {
 }
 
 export enum ProductStatus {
-ALL = 0, 
-DRAFT = 14800,
-PUBLISHED = 14801, 
-AWAITING_MODERATION = 14802,
-AWAITING_APPROVE_TO = 14803, 
-APPROVED = 14804, 
-ARCHIVED = 14805, 
-REQUIRE_IMPROVEMENT = 14806, 
-BLOCKED = 14807, 
+  ALL = 0,
+  DRAFT = 14800,
+  PUBLISHED = 14801,
+  AWAITING_MODERATION = 14802,
+  AWAITING_APPROVE_TO = 14803,
+  APPROVED = 14804,
+  ARCHIVED = 14805,
+  REQUIRE_IMPROVEMENT = 14806,
+  BLOCKED = 14807,
 }
 
 export enum ProductType {
-  NEW = 14701
+  NEW = 14701,
+}
+
+export enum PriceType {
+  KGS = 14600,
+  USD = 14601,
+}
+
+export enum ProductMode {
+  CREATE = "create",
+  UPDATE = "update",
 }
