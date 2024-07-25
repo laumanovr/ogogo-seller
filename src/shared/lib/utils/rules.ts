9062be618341cad2da45c477368c1f1c69338524
Ramazan <laumanov.r@gmail.com>
2024-07-25 18:46:21 +0600

    add localization for rule

--- src/shared/lib/utils/rules.ts
+++ src/shared/lib/utils/rules.ts
@@ -1,4 +1,9 @@
-// TODO: localize any error text. Write tests for rule functions
+import i18n from "@/shared/lib/plugins/i18n";
+
 export const requiredField = (value: any) => {
-  return (value && value.length >= 1) || Number(value) || "Обязательное поле";
+  return (
+    (value && value.length >= 1) ||
+    Number(value) ||
+    i18n.global.t("lang-ae6e60da-0ad7-4adb-af28-3e12950a0022")
+  );
 };
