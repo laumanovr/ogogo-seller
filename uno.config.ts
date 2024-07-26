6f8269cd601df3fd6995e6dde7e8aa4933a81701
Ramazan <laumanov.r@gmail.com>
2024-07-26 15:24:09 +0600

    modified uno-config

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,7 @@
 import { defineConfig } from "unocss";
 import { ogogoPreset } from "@tumarsoft/ogogo-ui";
-import presetUno from "@unocss/preset-uno";
 
 export default defineConfig({
-  // TODO: remove presetUno
-  presets: [presetUno(), ogogoPreset()],
-  rules: [
-    [/^gap-([\.\d]+)$/, ([_, num]) => ({ gap: `${num}px` })],
-    [/^font-size-(\d+)$/, ([_, size]) => ({ "font-size": `${size}px` })],
-  ],
+  presets: [ogogoPreset()],
+  rules: [],
 });
