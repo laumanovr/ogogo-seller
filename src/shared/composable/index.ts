a9314260c0df20fba89e10bb0bb460f373469f5c
Ramazan <laumanov.r@gmail.com>
2024-07-26 18:11:11 +0600

    add icon-path composable

--- src/shared/composable/index.ts
+++ src/shared/composable/index.ts
@@ -5,3 +5,7 @@ const authStore = useAuthStore();
 export const getFileById = (fileId: string) => {
   return `${axios.defaults.baseURL}File/FileById?id=${fileId}&sessionId=${authStore.getSessionId}`;
 };
+
+export const getFullIcon = (iconPath: string) => {
+    return `data:image/png;base64,${iconPath}`
+}
