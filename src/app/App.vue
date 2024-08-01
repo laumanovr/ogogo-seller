9eedab63f1fe55c3fb9bffb4b0cae6589aea56a6
Ramazan <laumanov.r@gmail.com>
2024-08-01 12:00:47 +0600

    changed into computed layout-component

--- src/app/App.vue
+++ src/app/App.vue
@@ -8,30 +8,23 @@
 <script lang="ts" setup>
 import Layout from "@/shared/ui/layouts/Layout.vue";
 import Empty from "@/shared/ui/layouts/Empty.vue";
-import { shallowRef, watch, computed } from "vue";
+import { computed } from "vue";
 import { SAlert } from "@tumarsoft/ogogo-ui";
 import { useAlertStore } from "@/shared/store/alert";
 import { useAuthStore } from "@/shared/store/auth";
-import { useRoute } from "vue-router";
 
 const authStore = useAuthStore();
 const alertStore = useAlertStore();
-let currentComponent = shallowRef(Empty);
-const route = useRoute();
 const alertItems = computed(() => alertStore.getAlertItems);
 
 const closeAlert = (id: string) => {
   alertStore.closeAlert(id);
 };
 
-watch(
-  () => route.path,
-  () => {
-    if (Boolean(authStore.getSessionId)) {
-      currentComponent.value = Layout;
-    } else {
-      currentComponent.value = Empty;
-    }
+const currentComponent = computed(() => {
+  if (Boolean(authStore.getSessionId)) {
+    return Layout;
   }
-);
+  return Empty;
+});
 </script>
