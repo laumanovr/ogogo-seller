cd1bcb49a1f620d8e73b0fd3293b7be0e9598185
Ramazan <laumanov.r@gmail.com>
2024-08-01 11:09:27 +0600

    modified rule

--- src/shared/lib/utils/rules.ts
+++ src/shared/lib/utils/rules.ts
@@ -3,6 +3,7 @@ import i18n from "@/shared/lib/plugins/i18n";
 export const requiredField = (value: any) => {
   return (
     (value && value.length >= 1) ||
+    (value !== undefined && value !== null && value !== "") ||
     Number(value) ||
     i18n.global.t("lang-ae6e60da-0ad7-4adb-af28-3e12950a0022")
   );
