e39241d9b1bd54d05a71aa3a3315c1b9d9f701f9
Ramazan <laumanov.r@gmail.com>
2024-08-01 11:08:55 +0600

    modified app

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,6 +1,5 @@
 <template>
   <component :is="currentComponent">
-    <Loader v-if="isLoading" />
     <SAlert :items="alertItems" @close="closeAlert" />
     <router-view />
   </component>
@@ -9,30 +8,22 @@
 <script lang="ts" setup>
 import Layout from "@/shared/ui/layouts/Layout.vue";
 import Empty from "@/shared/ui/layouts/Empty.vue";
-import Loader from "@/shared/ui/components/Loader.vue";
 import { shallowRef, watch, computed } from "vue";
 import { SAlert } from "@tumarsoft/ogogo-ui";
 import { useAlertStore } from "@/shared/store/alert";
-import { useLoaderStore } from "@/shared/store/loader";
 import { useAuthStore } from "@/shared/store/auth";
 import { useRoute } from "vue-router";
 
 const authStore = useAuthStore();
 const alertStore = useAlertStore();
-const loaderStore = useLoaderStore();
-
 let currentComponent = shallowRef(Empty);
-
 const route = useRoute();
-
 const alertItems = computed(() => alertStore.getAlertItems);
-const isLoading = computed(() => loaderStore.getLoadingState);
 
 const closeAlert = (id: string) => {
   alertStore.closeAlert(id);
 };
 
-// TODO: change component value to via computed
 watch(
   () => route.path,
   () => {
@@ -43,11 +34,4 @@ watch(
     }
   }
 );
-
-// const currentComponent = computed(() => {
-//   if (Boolean(getItem("sessionId"))) {
-//     return Layout;
-//   }
-//   return Empty;
-// });
 </script>
