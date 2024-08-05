21229e9d28f81e230d3b794507a4a78ea63abad0
Ramazan <laumanov.r@gmail.com>
2024-08-05 18:40:43 +0600

    modified seller-regis

--- src/pages/seller-registration/ui/SellerRegistration.vue
+++ src/pages/seller-registration/ui/SellerRegistration.vue
@@ -3,6 +3,5 @@
 </template>
 
 <script lang="ts" setup>
-// TOOD: use absolute path. example: import { SellerRegistrationWidget } from "@/widgets/seller-registration/SellerRegistrationWidget.vue";
-import SellerRegistrationWidget from "../../../widgets/seller-registration/SellerRegistrationWidget.vue";
+import SellerRegistrationWidget from "@/widgets/seller-registration/SellerRegistrationWidget.vue";
 </script>
